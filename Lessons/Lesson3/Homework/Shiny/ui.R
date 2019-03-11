63180d5fab1a87f8f7689a9ee624149703bfdd71
Miska0511 <47779343+Miska0511@users.noreply.github.com>
2019-03-11 19:58:46 +0100

    Update ui.R

--- Lessons/Lesson3/Homework/Shiny/ui.R
+++ Lessons/Lesson3/Homework/Shiny/ui.R
@@ -7,3 +7,40 @@ fluidPage(
   mainPanel(plotOutput("scatterPlot"))
   
   )
+################ Teoreticka cast ###################
+
+# Region Cergo dosahuje oproti regionu Belandia vyssi zisk, pri rovnakych vyskach nakladov.
+# Velke segmenty su oproti malym segmentom menej rizikove, teda aj hodnoty Premium a Expenses su mensie pre velke segmenty.
+# Najstrmsiu krivku vztahu Premium a Expenses ma poistenie domacnosti, lebo domacnosti su nakladnejsie oproti ostatnym typom poistenia. Je to sposobene vplyvom ovela vacsieho poctu faktorov, ktore sa v inych oblastiach neobjavuju. 
+# Z hladiska portolii, najyssie hodnoty Premium aj Expenses dosahuje Unit 7, lebo obsahuje narizikovejsie podniky. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
